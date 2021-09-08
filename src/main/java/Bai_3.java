9a9f75209e78ed99ca1ab323866fceb07118c615
ThanhTu0301 <thanhtuluu2002@gmail.com>
2021-09-08 10:19:26 +0700

    Init Project

---
+++ src/main/java/Bai_3.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Administrator
+ */
+public class Bai_3 {
+    public static void main(String[] args) {
+        System.out.println("Xin chào");
+        System.out.println("Mình tên là Thanh Tú");
+    }
+}
